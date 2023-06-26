3df4ac6e1f0aba0322eef07f01bd0d155cd543bc
Scott <109820423+8cott@users.noreply.github.com>
2023-06-26 17:21:04 -0400

    fixed login issue

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import { toast } from 'react-toastify';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
@@ -38,25 +37,35 @@ const Login = () => {
 
   const handleSuccess = (msg) => toast.success('🍹 Logged in successfully');
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const data = new FormData(e.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-    // Your login logic goes here
+    try {
+      const { data } = await axios.post(
+        'http://localhost:8000/login',
+        {
+          ...inputValue,
+        },
+        { withCredentials: true }
+      );
+      const { success, message, token } = data;
+      if (success) {
+        localStorage.setItem('jwt', token);
+        setIsLoggedIn(true);
+        handleSuccess(message);
+        setTimeout(() => {
+          navigate('/');
+        }, 1000);
+      } else {
+        handleError(message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
     setInputValue({
       ...inputValue,
       email: '',
       password: '',
     });
-    // Set the user as logged in and navigate to the home page
-    setIsLoggedIn(true);
-    handleSuccess('Logged in successfully');
-    setTimeout(() => {
-      navigate('/');
-    }, 1000);
   };
 
   return (
@@ -125,12 +134,12 @@ const Login = () => {
             </Button>
             <Grid container>
               <Grid item xs>
-                <Link href="#" variant="body2">
+                <LinkMui component={Link} to="/forgot-password" variant="body2">
                   Forgot password?
-                </Link>
+                </LinkMui>
               </Grid>
               <Grid item>
-              <LinkMui component={Link} to="/signup" variant="body2">
+                <LinkMui component={Link} to="/signup" variant="body2">
                   Don't have an account? Sign up
                 </LinkMui>
               </Grid>
