25757ddd338a76e8331766821cfc95877c8c8202
salmonte91 <stevenalmonte4@gmail.com>
2023-06-26 18:30:02 -0400

    styled cxreate drink

--- client/src/components/CreateDrink.jsx
+++ client/src/components/CreateDrink.jsx
@@ -3,6 +3,11 @@ import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import { AuthContext } from './AuthContext';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import { Box } from '@mui/material';
 
 const CreateDrink = () => {
   const navigate = useNavigate();
@@ -50,8 +55,9 @@ const CreateDrink = () => {
     e.preventDefault();
 
     if (!drink.image_url) {
-      drink.image_url = "https://images.unsplash.com/photo-1514362545857-3bc16c4c7d1b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80";
-    }  
+      drink.image_url =
+        'https://images.unsplash.com/photo-1514362545857-3bc16c4c7d1b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80';
+    }
 
     axios
       .post('http://localhost:8000/drinks', drink)
@@ -76,79 +82,65 @@ const CreateDrink = () => {
   };
 
   return (
-    <div className="CreateDrink">
-      <div className="container">
-        <div className="row">
-          <div>
-            <br />
-            <Link to="/">Show Drink List</Link>
-          </div>
-          <div>
-            <h1>Add Drink</h1>
-            <p className="lead text-center">Create new drink</p>
+    <Box 
+    sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      mb: 4,
+      marginTop:"5rem"
+    }}>
+    <Container>
+      <Typography variant="h2" align="center" variant="h5">
+        Create New Drink
+      </Typography>
 
-            <form noValidate onSubmit={onSubmit}>
-              <div className="form-group">
-                <input
-                  type="text"
-                  placeholder="Name of the Drink"
-                  name="drink_name"
-                  className="form-control"
-                  value={drink.drink_name}
-                  onChange={onChange}
-                />
-              </div>
-              <br />
+      <form noValidate onSubmit={onSubmit}>
+        <TextField
+          fullWidth
+          label="Name of the Drink"
+          name="drink_name"
+          value={drink.drink_name}
+          onChange={onChange}
+          margin="normal"
+          required
+        />
+        <TextField
+          fullWidth
+          label="Ingredients"
+          name="ingredients"
+          value={drink.ingredients}
+          onChange={onChange}
+          margin="normal"
+          required
+        />
+        <TextField
+          fullWidth
+          label="Recipe"
+          name="recipe"
+          value={drink.recipe}
+          onChange={onChange}
+          margin="normal"
+          required
+        />
+        <TextField
+          fullWidth
+          label="Image URL"
+          name="image_url"
+          value={drink.image_url}
+          onChange={onChange}
+          margin="normal"
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Submit
+        </Button>
+      </form>
 
-              <div className="form-group">
-                <input
-                  type="text"
-                  placeholder="Ingredients"
-                  name="ingredients"
-                  className="form-control"
-                  value={drink.ingredients}
-                  onChange={onChange}
-                />
-              </div>
-
-              <div className="form-group">
-                <input
-                  type="text"
-                  placeholder="Recipe"
-                  name="recipe"
-                  className="form-control"
-                  value={drink.recipe}
-                  onChange={onChange}
-                />
-              </div>
-
-              <div className="form-group">
-                <input
-                  type="text"
-                  placeholder="Image_URL"
-                  name="image_url"
-                  className="form-control"
-                  value={drink.image_url}
-                  onChange={onChange}
-                />
-              </div>
-
-              {/* <div className="form-group">
-                <input
-                  type="text"
-                  placeholder="Author"
-                  name="author"
-                  className="form-control"
-                  value={drink.author}
-                  onChange={onChange}
-                />
-              </div> */}
-              <input type="submit" />
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
+      <Typography variant="subtitle1" align="center" gutterBottom>
+        <Link to="/">Show Drink List</Link>
+      </Typography>
+    </Container>
+  </Box>
   );
 };
 
